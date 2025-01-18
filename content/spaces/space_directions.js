f323421571c0816edcc994c4e456b475a1a7f1f4
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2025-01-17 19:59:18 -0800

    Better lines

--- content/spaces/space_directions.js
+++ content/spaces/space_directions.js
@@ -7,17 +7,22 @@ class TempleSpaceDirectionsBuilder {
         const material = new THREE.LineBasicMaterial( { color: 0xccFFcc } );
 
         const points = [];
-        points.push( new THREE.Vector3( 0, 0, 0 ) );
-        points.push( new THREE.Vector3( 1, 0, 0 ) );
-        points.push( new THREE.Vector3( 0, 0, 1 ) );
-        points.push( new THREE.Vector3(-1, 0, 0 ) );
-        points.push( new THREE.Vector3( 0, 0, -1 ) );
+        this.addDirectionLines(points, 1.0);
+        this.addDirectionLines(points, 3.0);
 
         const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
         const line = new THREE.Line( geometry, material );
 
         parentScene.add( line );
+    }
+
+    addDirectionLines(points, radius = 1.0) {
+        points.push( new THREE.Vector3( 0, 0, 0 ) );
+        points.push( new THREE.Vector3( radius, 0, 0 ) );
+        points.push( new THREE.Vector3( 0, 0, radius ) );
+        points.push( new THREE.Vector3(-radius, 0, 0 ) );
+        points.push( new THREE.Vector3( 0, 0, -radius ) );
     }
 
 
